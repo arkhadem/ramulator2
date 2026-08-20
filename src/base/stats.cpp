2fadd95b13e7748dff43594d86c0d3bde08e00d2
Joseph Maheshe <maheshejs@gmail.com>
2026-08-20 10:20:59 -0700

    sort stats alphabetically since initially the hash map always gives a random printing order - rename stats for readable sorted output

--- src/base/stats.cpp
+++ src/base/stats.cpp
@@ -2,11 +2,24 @@
 
 namespace Ramulator {
 
-YAML::Emitter& operator << (YAML::Emitter& emitter, const Stats& s) {
-  for (auto [stat_name, stat_ptr] : s._registry) {
-    stat_ptr->emit_to(emitter);
-  }
-	return emitter;
+YAML::Emitter& operator<<(YAML::Emitter& emitter, const Stats& s) {
+    // collect keys
+    std::vector<std::string> keys;
+    keys.reserve(s._registry.size());
+    for (auto& kv : s._registry) {
+        keys.push_back(kv.first);
+    }
+
+    // sort keys alphabetically
+    std::sort(keys.begin(), keys.end());
+
+    // print in sorted order
+    for (auto& key : keys) {
+        auto* stat = s._registry.at(key);
+        stat->emit_to(emitter);
+    }
+    return emitter;
 }
+
 
 }        // namespace Ramulator
